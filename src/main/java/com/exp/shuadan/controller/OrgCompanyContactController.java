c29885d769591e674df0eded63d2de9db0737606
xiexinyuan <>
2020-10-29 00:07:40 +0800

    增加文件上传功能

--- src/main/java/com/exp/shuadan/controller/OrgCompanyContactController.java
+++ src/main/java/com/exp/shuadan/controller/OrgCompanyContactController.java
@@ -5,6 +5,7 @@ import com.alibaba.fastjson.JSONObject;
 import com.exp.shuadan.entity.CompanyInfo;
 import com.exp.shuadan.entity.OrgCompanyContact;
 import com.exp.shuadan.entity.ReturnMessage;
+import com.exp.shuadan.service.ExcelService;
 import com.exp.shuadan.service.OrgCompanyContactService;
 import com.seepine.http.util.HttpUtil;
 import org.apache.http.HttpResponse;
@@ -12,14 +13,17 @@ import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.io.InputStream;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -40,6 +44,9 @@ public class OrgCompanyContactController {
 
     @Autowired
     OrgCompanyContactService orgCompanyContactService;
+
+    @Autowired
+    ExcelService excelService;
 
     @RequestMapping("/getByCompName/{companyName}")
     public ReturnMessage getCompantContact(@PathVariable String companyName) throws Exception {
@@ -90,7 +97,7 @@ public class OrgCompanyContactController {
 //        }
     }
 
-    public CompanyInfo[] combineIndexSearch(String companyName) throws Exception {
+    private CompanyInfo[] combineIndexSearch(String companyName) throws Exception {
         HashMap<String, Object> map = new HashMap<>();
         map.put("token", sessionToken);
         map.put("type", "JSON");
@@ -109,7 +116,7 @@ public class OrgCompanyContactController {
         return result;
     }
 
-    public void getCompanyConact(String companyName) throws Exception {
+    private void getCompanyConact(String companyName) throws Exception {
         HashMap<String, Object> map = new HashMap<>();
         map.put("token", this.sessionToken);
         map.put("type", "JSON");
@@ -126,5 +133,34 @@ public class OrgCompanyContactController {
             for (int i = 0; i < ocArr.length; i++) {
                 this.add(ocArr[i]);
             }
+    }
+
+
+    /**
+     * 上传Excel文件并读取
+     * @param request
+     * @return
+     * @throws Exception
+     */
+    @PostMapping(value = "/upload")
+    @ResponseBody
+    public String uploadExcel(HttpServletRequest request) throws Exception {
+        MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+
+        MultipartFile file = multipartRequest.getFile("filename");
+        if (file.isEmpty()) {
+            return "文件不能为空";
+        }
+        InputStream inputStream = file.getInputStream();
+        List<List<Object>> list = excelService.getBankListByExcel(inputStream, file.getOriginalFilename());
+        inputStream.close();
+
+        for (int i = 0; i < list.size(); i++) {
+            List<Object> lo = list.get(i);
+            //TODO 随意发挥
+            System.out.println(lo);
+
+        }
+        return "上传成功";
     }
 }
---
+++ src/main/java/com/exp/shuadan/controller/ViewController.java
@@ -0,0 +1,12 @@
+package com.exp.shuadan.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+    @GetMapping("/upload")
+    public String upload(){
+        return "upload";
+    }
+}
